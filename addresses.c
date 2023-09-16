31a2f3985819d28c691d1b941148a1e47bf527e6
Grant Hall <grantclone@gmail.com>
2023-09-15 23:16:26 -0700

    use strcmp

--- addresses.c
+++ addresses.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 #include <cs50.h>
 
 int main(void)
 {
-  char *tmp  = "potato!";
-  printf("go to location at pointer and print character: %c\n", *tmp);
-  printf("print the whole string: %s\n", tmp);
-  printf("value of pointer: %p\n" , tmp);
-  printf("address of pointer: %p\n" , &tmp);
+  // get 2 strings
+  char *s = get_string("s: ");
+  char *t = get_string("t: ");
 
-  string s = "carrot!";
-  printf("change the electrical charges of the silicon transistors starting at somewhere that is freed up - go to hexidecimal value of the location of the first byte taken up by the string carrot - print the characters until you hit a \\0: %s\n", s);
-
+  // compare strings addresses
+  if (strcmp(s, t) == 0)
+  {
+    printf("Same\n");
+  }
+  else
+  {
+    printf("not the same :/\n");
+  }
 }
