7697c5f3918d6f76c19e91c07d3e91bbe6ab6940
Grant Hall <grantclone@gmail.com>
2023-09-15 22:21:14 -0700

    added explanations

--- addresses.c
+++ addresses.c
@@ -3,11 +3,13 @@
 
 int main(void)
 {
-  string s  = "potato";
-  printf("%p\n" , s);
-  printf("%p\n" , &s[0]);
-  printf("%p\n" , &s[1]);
-  printf("%p\n" , &s[2]);
-  printf("%p\n" , &s[3]);
-  printf("%p\n" , &s[4]);
+  char *tmp  = "potato!";
+  printf("go to location at pointer and print character: %c\n", *tmp);
+  printf("print the whole string: %s\n", tmp);
+  printf("value of pointer: %p\n" , tmp);
+  printf("address of pointer: %p\n" , &tmp);
+
+  string s = "carrot!";
+  printf("change the electrical charges of the silicon transistors starting at somewhere that is freed up - go to hexidecimal value of the location of the first byte taken up by the string carrot - print the characters until you hit a \\0: %s\n", s);
+
 }
